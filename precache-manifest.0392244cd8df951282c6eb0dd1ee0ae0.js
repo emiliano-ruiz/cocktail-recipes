e7e0f03151d78a6cf92ff6d22fdee4f46bf68bb3
Emiliano Ruiz Carletti <eruiz42100@gmail.com>
2020-10-01 22:37:54 -0300

    Updates

---
+++ precache-manifest.0392244cd8df951282c6eb0dd1ee0ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e088192a78cbc9ae9329cd7ae637c36e",
+    "url": "/cocktail-recipes/index.html"
+  },
+  {
+    "revision": "d72562ce8924860b7e85",
+    "url": "/cocktail-recipes/static/css/main.682aa4b5.chunk.css"
+  },
+  {
+    "revision": "f6b30204e04ff6521059",
+    "url": "/cocktail-recipes/static/js/2.3fe06ca7.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/cocktail-recipes/static/js/2.3fe06ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72562ce8924860b7e85",
+    "url": "/cocktail-recipes/static/js/main.da442721.chunk.js"
+  },
+  {
+    "revision": "3a48af6734d2ba540fe9",
+    "url": "/cocktail-recipes/static/js/runtime-main.315b4d67.js"
+  }
+]);
